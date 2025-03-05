44390c966a88fa098c8203a025b320823e4e34a2
Shlok Ghimire <foot123trick@gmail.com>
2025-03-05 20:01:15 +0545

    Code

--- web.php
+++ web.php
@@ -1,7 +1,17 @@
 <?php
-
 $servername="localhost";
-
-
-
+$username = "root";
+$password = " ";
+$connect =  mysqli_connect($servername,$username,$password);
+if(!$scan){
+    die("Connection Failed: .mysqli_connect-error() ");
+}
+$sql="CREATE DATABASE webTech";
+if(mysql_query ($connect,$sql));{
+    echo"Database created sucessfully.";
+}
+else{
+    echo"Error:".mysqli_error($connect);
+}
+mysqli_close($connect)
 ?>
