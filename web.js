172bfc2b99ab417dc65370bfe1ddf1564741c1b1
Shlok Ghimire <foot123trick@gmail.com>
2025-03-05 21:59:09 +0545

    All

--- web.js
+++ web.js
@@ -1,6 +1,18 @@
 function validate(){
     var a= document.getElementById("n1").value;
     var b=document.getElementById("n2").value;
-    var c=document.getElementById("n3").value;
-    var d =document.getElementById("n4").value;
+    var gender = document.querySelector(input[name=gender].checked);
+
+    if(a=="" || b==""){
+        alert("All Fields Are Mandatory!");
+        return false;
+    }
+    if(b.length != 10 || NaN(b)){
+        alert("Please Enter valid 10 digits.");
+        return false;
+    }
+    if(!gender){
+        alert("Please select your gender.");
+        return false;
+    }
 }
--- web.php
+++ web.php
@@ -7,11 +7,13 @@ if(!$scan){
     die("Connection Failed: .mysqli_connect-error() ");
 }
 $sql="CREATE DATABASE webTech";
-if(mysql_query ($connect,$sql));{
+
+if(mysqli_query ($connect,$sql));{
     echo"Database created sucessfully.";
 }
+
 else{
-    echo"Error:".mysqli_error($connect);
+    echo"Error:.mysqli_error($connect)";
 }
 mysqli_close($connect)
 ?>
