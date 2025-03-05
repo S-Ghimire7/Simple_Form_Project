a5cc6c02661fd07a6094d063d8e0d821ab5a6c6c
Shlok Ghimire <foot123trick@gmail.com>
2025-03-05 20:33:11 +0545

    All

---
+++ web.js
@@ -0,0 +1,6 @@
+function validate(){
+    var a= document.getElementById("n1").value;
+    var b=document.getElementById("n2").value;
+    var c=document.getElementById("n3").value;
+    var d =document.getElementById("n4").value;
+}
