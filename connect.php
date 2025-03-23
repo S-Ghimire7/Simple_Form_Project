7b4cfbfe98c97c7dd7338098c91f7f21c005b905
Shlok Ghimire <foot123trick@gmail.com>
2025-03-23 21:28:03 +0545

    Final code prototype

--- connect.php
+++ connect.php
@@ -1,10 +1,34 @@
 <?php
 // MADE BY SHLOK GHIMIRE
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "kumar1";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $user = trim($_POST['user']);
+    $email = trim($_POST['email']);
+    $age = (int)$_POST['age']; 
+
+    $servername = 'localhost';
+    $serverroot = 'root';
+    $serverpass = '';
+    $dbname = "Final_test";
+    $conn = mysqli_connect($servername, $serverroot, $serverpass, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        $stmt = $conn->prepare("INSERT INTO registration (user, email, age) VALUES (?, ?, ?)");
+        if ($stmt) {
+            $stmt->bind_param("ssi", $user, $email, $age);
+            if ($stmt->execute()) {
+                echo "Registration successful";
+            } else {
+                echo "Error executing statement: " . $stmt->error;
+            }
+            mysqli_close($stmt)
+        } else {
+            echo "Error preparing statement: " . $conn->error;
+        }
+        mysqli_close($conn);
+    }
+} else {
+    echo "Invalid request method.";
 }
+?>
